09f043a92c17b5d491d1305c87daf61d6b936262
Zharfan Akbar <zharfan.akbar104@gmail.com>
2023-02-26 11:01:40 +0700

    Change 2 word for tags to 1

--- lib/features/chat/utils/extract_conversation.dart
+++ lib/features/chat/utils/extract_conversation.dart
@@ -3,7 +3,7 @@ String extractConversation(String conversation) {
     Read the following conversation and extract principles about the user from it. 
     What are some common themes, facts, preferences, personal information, or observations about the user.
     List out the principles and write them in very very brief sentences, also add 3 tags for each principle,
-    for the tags, use maximum 2 word with underscore and lowercased
+    for the tags, use maximum 1 word with underscore and lowercased
     and then map each principles with the tag in this JSON format:
     {
       "principles": [
